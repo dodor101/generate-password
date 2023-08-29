9bf316a8a58fe80fb15187a6c848e9d5e59076ca
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2023-08-29 12:37:09 -0500

    Add generate password in js file and event listieners on form element

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,13 @@
-
-
+// import dom element
+const uppercaseEl = document.getElementById('uppercase');
+const numberEl = document.getElementById('number');
+const lowercaseEl = document.getElementById('lowercase');
+const specialEl = document.getElementById('special');
+const lengthEl = document.getElementById('length');
+const submitBtn = document.querySelector('.submit-btn');
+const inputEl = document.querySelector('.number-input');
+const form = document.querySelector('.form');
+const displayPassword = document.querySelector('#display-password');
 // create a function that returns different characters from fromCharCode based on required parameters/argument
 
 const generateCharacters = (startPosition, endPosition) => {
@@ -17,21 +25,53 @@ const generateCharacters = (startPosition, endPosition) => {
 const uppercaseLetters = generateCharacters(65, 90);
 const lowercaseLetters = generateCharacters(97, 122);
 const specialCharacters = generateCharacters(33, 47);
+const numbers = generateCharacters(48, 57);
+
+inputEl.addEventListener('input', syncPasswordLength);
+lengthEl.addEventListener('input', syncPasswordLength);
+
+function syncPasswordLength(e) {
+  const { value } = e.target;
+  inputEl.value = value;
+  lengthEl.value = value;
+}
 
 // create an array of random characters
-function generatePassword() {
-  let availableChars = [...uppercaseLetters, ...lowercaseLetters, ...specialCharacters];
-  console.log('🚀 ~ file: script.js:24 ~ generatePassword ~ availableChars:', availableChars);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  const passwordLength = 10;
+  const passwordLength = inputEl.value;
+  const includeUppercase = uppercaseEl.checked;
+  const includeNumbers = numberEl.checked;
+  const includeSpecial = specialEl.checked;
 
+  const password = generatePassword(passwordLength, includeUppercase, includeNumbers, includeSpecial);
+  displayPassword.innerText = password;
+});
+
+/* Write a generatePassword function thats take
+num of parameter and conditionally render content on the page.
+*/
+function generatePassword(passwordLength, includeUppercase, includeNumbers, includeSpecial) {
   let password = '';
-  for (let i = 0; i < passwordLength; i++) {
-    const randomIndex = Math.floor(Math.random() * availableChars.length);
-    password += availableChars[randomIndex];
+
+  let availableLetters = lowercaseLetters;
+
+  if (includeUppercase) {
+    availableLetters = [...availableLetters, ...uppercaseLetters];
   }
+
+  if (includeSpecial) {
+    availableLetters = [...availableLetters, ...specialCharacters];
+  }
+  if (includeNumbers) {
+    availableLetters = [...availableLetters, ...numbers];
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomIndex = Math.floor(Math.random() * availableLetters.length);
+    password += availableLetters[randomIndex];
+  }
+
   return password;
 }
-const password = generatePassword();
-
-console.log(password);
